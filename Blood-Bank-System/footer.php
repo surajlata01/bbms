5b66b48d38e8baae52eab28e3aadfb4076a03122
Suraj Lata <112932002+surajlata01@users.noreply.github.com>
2024-08-05 21:29:09 +0530

    Add files via upload

---
+++ Blood-Bank-System/footer.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+  <style>
+  #footer {
+
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 75px;
+  background-color:#000000;
+  color:white;
+  text-align: center;
+}
+  </style>
+</head>
+<body>
+  <div id="footer" >
+  <b><center>Copyright © 2023 <a href="http://chaudharysurajlata.w3spaces.com" target="_blank" rel="noopener noreferrer" style="color: white;">Suraj Lata</a><br>
+  Blood Bank System
+  <br>
+  All Rights Reserved
+  </center>
+  </div>
+
+
+</body>
+
+</html>
